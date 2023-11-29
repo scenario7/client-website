445827458969a58fa95f52c74ba180fb26f9bad5
kevinthomas <kevvin.thomas@gmail.com>
2023-11-29 17:51:47 +0530

    edited navbar

--- src/components/CustomFooter.jsx
+++ src/components/CustomFooter.jsx
@@ -10,8 +10,12 @@ const CustomFooter = () => {
             <h1 className='font-sans text-left lg:text-md md:text-md text-sm text-blue-200'>PO Box 7311 Dubai, UAE</h1>
         </div>
         <div className='flex flex-col items-end'>
+          <a href='tel:+971508263954'>
             <h1 className='font-sans text-white lg:text-lg md:text-lg text-xs'>+971 50 8263954</h1>
+          </a>
+          <a href='mailto:info@ltitsolutions.com'>
             <h1 className='font-sans text-white lg:text-lg md:text-lg text-xs'>info@ltitsolutions.com</h1>
+          </a>
         </div>
     </div>
     <div className="bg-blue-950 text-white py-3">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,29 +6,38 @@ const NavBar = () => {
     const location = useLocation()
 
   return (
-    <div className='bg-stone-900 px-2 py-3 flex flex-col items-center'>
+    <div className='bg-stone-900 px-8 py-3 flex flex-col items-center'>
+        <div className="flex items-center justify-between w-full">
         <a href="/">
-            <h1 className='font-serif text-white text-3xl py-5'>LT IT Solutions</h1>
-            {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/967px-Emirates_logo.svg.png" alt="" className='object-contain h-32 py-5'/> */}
+            <h1 className='font-serif text-white text-3xl py-5 text-left'>LT IT Solutions</h1>
         </a>
-        <div className='flex justify-center pb-3'>
+        <div className='flex flex-col items-end'>
+          <a href='tel:+971508263954'>
+            <h1 className='font-sans text-white lg:text-lg md:text-lg text-xs'>+971 50 8263954</h1>
+          </a>
+          <a href='mailto:info@ltitsolutions.com'>
+            <h1 className='font-sans text-white lg:text-lg md:text-lg text-xs'>info@ltitsolutions.com</h1>
+          </a>
+        </div>
+        </div>
+        <div className='flex items-start w-full justify-start pb-3'>
             <a href="/">
-            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 hover:font-semibold py-3`} href="/projects">
+            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 py-2 rounded-full`} href="/projects">
                 Home
             </button>
             </a>
             <a href="/solutions">
-            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/solutions" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 hover:font-semibold py-3`} href="/projects">
+            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/solutions" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 py-2 rounded-full`} href="/projects">
                 Solutions
             </button>
             </a>
             <a href="/verticals">
-            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/verticals" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 hover:font-semibold py-3`} href="/projects">
+            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/verticals" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 py-2 rounded-full`} href="/projects">
                 Verticals
             </button>
             </a>
             <a href="/contact">
-            <button className={`font-sans text-gray-400 hover:text-white ${location.pathname === "/contact" ? 'text-white bg-stone-800' : 'text-gray-400 bg-transparent'} px-3 hover:font-semibold py-3`} href="/projects">
+            <button className='font-sans hover:bg-blue-600 hover:text-white transition ease-out bg-white text-blue-600 px-3 py-2 rounded-full' href="/projects">
                 Contact
             </button>
             </a>
