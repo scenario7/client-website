be3c3e037485ad5dcf34880e9bc7bb7aebeaf7f1
kevinthomas <kevvin.thomas@gmail.com>
2023-12-25 21:06:37 +0530

    added company logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import companylogo from '../images/companylogo.png';
-
 const NavBar = () => {
 
     const location = useLocation()
